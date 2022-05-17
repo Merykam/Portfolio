98122cf4671de82f63697f8011edc8c8d01e9903
kamaych <kamaychmeryem@gmail.com>
2022-05-17 14:20:57 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,36 @@
+export const home = {
+    domain: 'Merykam.github.io',
+    name: 'Mariem',
+    subtitle: 'A Freelance Web Developer from Morocco.',
+    email: 'kamaychmeryem@gmail.com',
+    desc: 'A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.'
+};
+
+export const socials = {
+    // Only usernames.
+    
+    github: 'Merykam',
+    instagram: 'meryemkamaych'
+};
+
+export const about = {
+    aboutMe:
+        '🙏 Hello, I am Mariem kamaych from Morocco, I am a Full Stack Developer.',
+    
+    quote: 'Write code that changes Lives...',
+    skills: [
+        'nextjs',
+        'react',
+        'python',
+        'css3', 
+        'django',
+        'html5',
+        'javascript',
+        'jquery',
+        'nodejs',
+       
+    ],
+    tools: ['github', 'photoshop', 'visualstudio',]
+};
+
+
